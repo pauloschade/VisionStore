f8adba34cc3c475720966604d182580675732005
pauloschade <paulochade0505@gmail.com>
2024-05-14 15:33:51 -0300

    Initial Commit

---
+++ my-first-app/my_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_appApp.swift
+//  my-first-app
+//
+//  Created by Paulo Tech on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct my_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
